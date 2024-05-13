874164d0d3f178fd2581c803d7ee0000c84baacb
loklok2 <loklok2@naver.com>
2024-05-13 18:00:09 +0900

    5rdweek/day1

--- hong/Python_work/chapter_08/partial_programs/printing_models_1_two_functions.py
+++ hong/Python_work/chapter_08/partial_programs/printing_models_1_two_functions.py
@@ -18,4 +18,6 @@ unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
 print_models(unprinted_designs, completed_models)
+print(f"원래변수 출력={unprinted_designs}")
+print(f"수정여부 출력={completed_models}")
 show_completed_models(completed_models)
